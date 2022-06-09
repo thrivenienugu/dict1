9b69c9a84011643f8493c8765db76a4907916e68
thrivenienugu <enuguthriveni@gmail.com>
2022-06-09 21:10:30 +0200

    define a new function main() that contains the entire while loop

--- dict.py
+++ dict.py
@@ -37,21 +37,24 @@ def read_print():
     DELETE - delete a contact
     LIST   - list all phone numbers
     QUIT   - quit the program''')
-read_print()   
-while True: ## REPL - Read Execute Program Loop
-    cmd = input("Command: ")
-    if cmd == "list":
-        print(read_dict(conn))
-    elif cmd == "add":
-        name = input("  Word: ")
-        phone = input("  Translation: ")
-        add_word(conn, name, phone)
-        print(f" Added word {name}")
-    elif cmd == "delete":
-        ID = input("  ID: ")
-        delete_word(conn, ID)
-        print(f" Deleted {ID}")
-    elif cmd == "quit":
-        save_dict(conn)
-        print(f" save {conn}")
-        exit()
+   
+def main():
+    while True: ## REPL - Read Execute Program Loop
+        cmd = input("Command: ")
+        if cmd == "list":
+            print(read_dict(conn))
+        elif cmd == "add":
+            name = input("  Word: ")
+            phone = input("  Translation: ")
+            add_word(conn, name, phone)
+            print(f" Added word {name}")
+        elif cmd == "delete":
+            ID = input("  ID: ")
+            delete_word(conn, ID)
+            print(f" Deleted {ID}")
+        elif cmd == "quit":
+            save_dict(conn)
+            exit()
+
+read_print()
+main()
