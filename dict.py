16cc3d85836d3e7475a2b0f327b651fa6f5b8611
thrivenienugu <enuguthriveni@gmail.com>
2022-06-09 20:27:19 +0200

    Added command lines to each function

--- dict.py
+++ dict.py
@@ -5,13 +5,16 @@ conn = psycopg2.connect(
    user="phone",
    password="Dev201801"
 )
-
+# read_dict: returns the list of all dictionary entries:
+#   argument: C - the database connection.
 def read_dict(C):
     cur = C.cursor()
     cur.execute("SELECT id, word, translation FROM dictionary;")
     rows = cur.fetchall()
     cur.close()
     return rows
+# add_work: add the values to the table:
+#   argument: C - the database connection.
 def add_word(C, word, translation):
     cur = C.cursor()
     cur.execute(f"INSERT INTO dictionary (word, translation) VALUES ('{word}', '{translation}');")
@@ -20,10 +23,14 @@ def delete_word(C, ID):
     cur = C.cursor()
     cur.execute(f"DELETE FROM dictionary WHERE id = '{ID}';")
     cur.close()
+# save_dict: save the list of all dictionary entries:
+#   argument: C - the database connection.
 def save_dict(C):
     cur = C.cursor()
     cur.execute("COMMIT;")
     cur.close()
+# read_print: print the list of all dictionary entries:
+#   argument: C - the database connection.
 def read_print():
     print('''Hello and welcome to the phone list, available commands:
     ADD    - add a phone number
