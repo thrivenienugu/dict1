e75a98a4ca9349bc46c933f0bec67337b49caa66
thrivenienugu <enuguthriveni@gmail.com>
2022-06-09 20:34:20 +0200

    change C to conn in the python function

--- dict.py
+++ dict.py
@@ -7,26 +7,26 @@ conn = psycopg2.connect(
 )
 # read_dict: returns the list of all dictionary entries:
 #   argument: C - the database connection.
-def read_dict(C):
-    cur = C.cursor()
+def read_dict(conn):
+    cur = conn.cursor()
     cur.execute("SELECT id, word, translation FROM dictionary;")
     rows = cur.fetchall()
     cur.close()
     return rows
 # add_work: add the values to the table:
 #   argument: C - the database connection.
-def add_word(C, word, translation):
-    cur = C.cursor()
+def add_word(conn, word, translation):
+    cur = conn.cursor()
     cur.execute(f"INSERT INTO dictionary (word, translation) VALUES ('{word}', '{translation}');")
     cur.close()
-def delete_word(C, ID):
-    cur = C.cursor()
+def delete_word(conn, ID):
+    cur = conn.cursor()
     cur.execute(f"DELETE FROM dictionary WHERE id = '{ID}';")
     cur.close()
 # save_dict: save the list of all dictionary entries:
 #   argument: C - the database connection.
-def save_dict(C):
-    cur = C.cursor()
+def save_dict(conn):
+    cur = conn.cursor()
     cur.execute("COMMIT;")
     cur.close()
 # read_print: print the list of all dictionary entries:
